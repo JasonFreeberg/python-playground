bc64f7bc0d145a036d4aab4a73d4cf4d88c9dd91
bernie-graves <75196548+bernie-graves@users.noreply.github.com>
2021-07-05 12:31:47 -0700

    added testFile.py

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ def triple(num):
     return num * 3
 
 
-def test_triple():
-    assert triple(4) == 12
-    assert triple(0) == 0
-
-
 if __name__ == '__main__':
     print('Hello world!')
 
---
+++ testFile.py
@@ -0,0 +1,6 @@
+from main import triple
+
+
+def test_triple():
+    assert triple(4) == 12
+    assert triple(0) == 0
