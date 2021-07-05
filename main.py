42b4e41a21a2af4a96b930347e3277b54588df90
bernie-graves <75196548+bernie-graves@users.noreply.github.com>
2021-07-05 12:26:28 -0700

    pytest workflow

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
 
+
+def triple(num):
+    return num * 3
+
+
+def test_triple():
+    assert triple(4) == 12
+    assert triple(0) == 0
+
+
 if __name__ == '__main__':
     print('Hello world!')
-    
+
     response = requests.get('https://swapi.dev/api/people/1')
     print(response.json())
-
